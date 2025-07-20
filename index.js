c2e48946a87d2a5850ca1aa4dc543938f23aa20f
Rahul Kumar Singh <rs3448397@gmail.com>
2025-07-20 11:42:12 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import env from "dotenv";
 
 env.config();
 
-const db = new pg.Client({
+const db = new pg.Pool({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
